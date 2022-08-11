7dc4ad2bb8c1c936f64006341fef3b0a40cc6279
Laura R <laualexandrar@gmail.com>
2022-08-11 01:01:33 -0700

    adding more functions

--- calculator.js
+++ calculator.js
@@ -9,25 +9,77 @@ class Calculator {
         this.previousOperand = ''
         this.operation = undefined;
   }
-  
   delete() {
-    
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
   appendNumber(number) {
-    this.currentOperand = number;
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString();
 
   }
-  chooseOperand(operation) {
-    
+  chooseOperation(operation) {
+    if(this.currentOperand == '') return
+    if(this.previousOperand !== '') {
+      this.compute()
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = ''
   }
   compute() {
-  
+  let computation 
+  const prev = parseFloat(this.previousOperand)
+  const current = parseFloat(this.currentOperand)
+  if(isNaN(prev) || isNaN(current)) return
+  switch (this.operation){
+    case '+':
+      computation = prev + current
+      break
+    case '-':
+      computation = prev - current
+      break
+    case '*':
+      computation = prev * current
+      break
+    case '÷':
+      computation = prev / current
+      break
+      default:
+        return
+    }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
   }
+ 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand
+    this.currentOperandTextElement.innerText =
+      this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText =
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
+    }
   }
 }
-
 const numberButtons = document.querySelectorAll('[data-button-number]');
 const operationButton = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -45,6 +97,26 @@ numberButtons.forEach(button => {
   });
 });
 
+operationButton.forEach(button => {
+  button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText);
+      calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener('click', button => {
+  calculator.compute();
+  calculator.updateDisplay();
+})
+
+dataClearButton.addEventListener('click', button => {
+  calculator.clear();
+  calculator.updateDisplay();
+})
+dataDeleteButton.addEventListener('click', button => {
+  calculator.delete();
+  calculator.updateDisplay();
+})
 
 
 
